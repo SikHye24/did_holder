f7589226a5265fe94c13166ae801590b71c09d81
sikhye1977 <sikhye1977@naver.com>
2024-07-24 03:43:57 +0900

    add credo-library

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,20 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+// const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
+// /**
+//  * Metro configuration
+//  * https://facebook.github.io/metro/docs/configuration
+//  *
+//  * @type {import('metro-config').MetroConfig}
+//  */
+// const config = {};
+
+// module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+//위는 기존 설정
+
+
+
+//아래는 지피티 수정
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 /**
  * Metro configuration
@@ -6,6 +22,11 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
+const defaultConfig = getDefaultConfig(__dirname);
+
+// 기본 설정의 resolver.sourceExts에 'cjs' 확장자를 추가합니다.
+defaultConfig.resolver.sourceExts.push('cjs');
+
 const config = {};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = mergeConfig(defaultConfig, config);
